e48fedceea32fdea11f5be4e38b26b5a694f2256
songlairui <cnre@live.cn>
2019-04-27 12:25:03 +0800

    init with apollo, rx

--- src/main.js
+++ src/main.js
@@ -2,11 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueRx from 'vue-rx';
+import { createProvider } from './vue-apollo'
+
+Vue.use(VueRx);
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  apolloProvider: createProvider(),
   render: h => h(App)
 }).$mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+
+  pluginOptions: {
+    apollo: {
+      enableMocks: false,
+      enableEngine: false
+    }
+  }
 }
