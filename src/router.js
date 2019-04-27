769df7c0567f995f1753d9fb69eb0e22a38a1651
songlairui <cnre@live.cn>
2019-04-27 12:35:53 +0800

    Add LaunchPad

--- src/router.js
+++ src/router.js
@@ -10,16 +10,24 @@ export default new Router({
   routes: [
     {
       path: '/',
+      redirect: '/launch-pad'
+    },
+    {
+      path: '/vue-cli-home',
       name: 'home',
       component: Home
     },
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: () =>
+        import(/* webpackChunkName: "about" */ './views/About.vue')
+    },
+    {
+      path: '/launch-pad',
+      name: 'launchpad',
+      component: () =>
+        import(/* webpackChunkName: "launchpad" */ './views/LaunchPad.jsx')
     }
   ]
 })
